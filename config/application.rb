45c6bdafa9c9d7152a29a821407669eab6e27ad0
ohno <ddeeppaa.gogo@gmail.com>
2021-02-14 12:26:32 +0900

    Finished rspec

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,12 @@ module BakeUp
 
     config.generators do |g|
      g.test_framework false  # testファイルは生成しない
-   end
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+     g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      end
+    end
   end
 end
