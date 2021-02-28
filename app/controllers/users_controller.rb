899bff1aa8d13190a2995de8e1c43adfe6bbc375
ohno <ddeeppaa.gogo@gmail.com>
2021-02-28 14:38:55 +0900

    createメソッド作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,24 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Bake upへようこそ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
