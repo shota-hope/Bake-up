540343ad5d1c5a43e1bff9f633736d971cf91ddf
ohno <ddeeppaa.gogo@gmail.com>
2021-02-28 19:02:29 +0900

    ログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
  root 'static_pages#home'
- get :about, to: 'static_pages#about'
- get :signup, to: 'users#new'
+ get       :about,  to: 'static_pages#about'
+ get       :signup, to: 'users#new'
  resources :users
+
+ get    :login,   to:'sessions#new'
+ post   :login,   to:'sessions#create'
+ delete :logout,  to:'sessions#destroy'
 end
