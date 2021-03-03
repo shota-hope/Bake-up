cf7f7ae33ba6931a026de1bb6eec2056ca4aa6b6
ohno <ddeeppaa.gogo@gmail.com>
2021-03-03 21:49:34 +0900

    テスト追記(ログイン)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Bake upへようこそ！"
       redirect_to @user
     else
       render 'new'
     end
   end
-  
+
   private
 
   def user_params
